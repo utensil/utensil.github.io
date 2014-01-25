4d3f0ae2119581447b997003a6296d3a53fea487
utensil <utensilcandel@gmail.com>
2014-01-25 12:37:55 +0800

    travis auto deploy try 2

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,8 +15,24 @@ bundle exec middleman build
 
 lastCommit=$(git log --oneline | head -n 1)
 
+# push to remote
+
 cd build
+
+echo "set up $GH_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
+export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
+git branch -a
+echo "STATUS"
+git status
+git remote rename origin old
+echo "remotes pre pre-authorized remote url"
+git remote -v
+git remote add origin $REPO_URL
+git config remote.origin.url $REPO_URL
+
 git add -f .
-git commit -m $lastCommit
+git commit -m "$lastCommit"
 git config remote.origin.url
 git push origin
