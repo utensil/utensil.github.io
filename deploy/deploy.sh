dabceb297c235a875669c9b0978a4f082abef9e8
utensil <utensilcandel@gmail.com>
2014-01-25 12:26:08 +0800

    travis auto deploy try 1

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# check out master to "build" and clean up
+
+git clone --depth=50 --branch=master git://github.com/utensil/utensil.github.com.git build
+(
+  cd build
+  # remove all files except .git and .travis.yml
+  ls -1|xargs rm -rf
+)
+
+# build site to "build"
+
+bundle exec middleman build
+
+lastCommit=$(git log --oneline | head -n 1)
+
+cd build
+git add -f .
+git commit -m $lastCommit
+git config remote.origin.url
+git push origin
--- deploy/pre_deploy.sh
+++ deploy/pre_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "BUMP HELLO WORLD set up $GH_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
+echo "set up $GH_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
 export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
@@ -13,12 +13,4 @@ git remote -v
 git remote add origin $REPO_URL
 git config remote.origin.url $REPO_URL
 
-echo "DEBUG, cd build"
-test -d build && (
-  cd build
-  echo -n "user.email"
-  git config user.email
-  echo -n "user.name"
-  git config user.name
 
-) || echo "fresh build, no build directory"
