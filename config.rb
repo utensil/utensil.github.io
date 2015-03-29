0c42bd60e2182af90a419a7f21e8abc92ae90c5f
utensil <utensilcandel@gmail.com>
2015-03-29 11:54:03 +0800

    Update config in a 4.0 preferred way

--- config.rb
+++ config.rb
@@ -1,10 +1,6 @@
-### 
+###
 # Compass
 ###
-
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
 
 # Change Compass configuration
 # compass_config do |config|
@@ -12,45 +8,37 @@
 # end
 
 ###
-# Haml
-###
-
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-###
-# Page command
+# Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
 ###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -59,14 +47,30 @@
 #   end
 # end
 
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
+# set :css_dir, 'stylesheets'
 
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
+# set :js_dir, 'javascripts'
 
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
+# set :images_dir, 'images'
+
+# Build-specific configuration
+# configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+# end
+
 
 set :encoding,    "utf-8"
 
@@ -100,13 +104,16 @@ set :markdown, :tables => true,
 set :md, :layout_engine => :haml
 set :haml, :layout_engine => :haml #, :encoding => 'utf-8'
 
-with_layout 'tech_layout.haml' do
-  page "/tech/*.html"
-end
+# with_layout 'tech_layout.haml' do
+#   page "/tech/*.html"
+# end
 
-with_layout 'writings_layout.haml' do
-  page "/writings/*.html"
-end
+# with_layout 'writings_layout.haml' do
+#   page "/writings/*.html"
+# end
+
+page "/tech/*.html", :layout => 'tech_layout'
+page "/writings/*.html", :layout => 'writings_layout'
 
 require "middleman-blog"
 activate :blog do |tech_blog|
