248d2748153240c3305079d4c6a311222d4720ec
utensil <utensilcandel@gmail.com>
2015-03-30 00:10:54 +0800

    Improve deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -23,6 +23,7 @@ echo "set up $GH_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
 export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
+git config --global push.default simple
 git branch -a
 echo "STATUS"
 git status
@@ -32,7 +33,7 @@ git remote -v
 git remote add origin $REPO_URL
 git config remote.origin.url $REPO_URL
 
-git add -f .
+git add -a -f .
 git rm $(git ls-files --deleted|grep -v README.md)
 git commit -m "$lastCommit"
 git push -q origin
