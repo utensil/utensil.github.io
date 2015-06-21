fe8d9ed5b68d5bdf91513bd1142efb31107e138c
utensil <utensilcandel@gmail.com>
2015-06-21 03:33:54 +0000

    Import new blogs from diandian

--- util/export_diandian.rb
+++ util/export_diandian.rb
@@ -2,43 +2,43 @@
 require './downmark_it'
 require './pinyin.rb'
 require 'open-uri'
-require 'nokogiri'
+require 'oga'
 
 pinyin = PinYin::PinYin.instance
 # paste the newest link here
-next_page_link = 'http://utensil-river.diandian.com/post/2014-08-08/40062436157'
-stop_link = 'http://utensil-river.diandian.com/post/2014-07-22/40062272726'
-doc = Nokogiri::HTML(open(next_page_link).read)
+next_page_link = 'http://utensil-river.diandian.com/post/2015-04-23/40066253870'
+stop_link = 'http://utensil-river.diandian.com/post/2014-08-08/40062436157'
+doc = Oga.parse_html(open(next_page_link).read)
 
 while true do
-  (doc/'.text-post').each do |post|
-    title = (post/'h2').inner_text.strip
+  doc.css('.text-post').each do |post|
+    title = post.css('h2')[0].inner_text.strip
     puts next_page_link
 
     #puts pinyin.to_pinyin(title, separator = '-', tone = true) 
     file_name = ''
 
-    datetime = (post/'.date').inner_text.strip.gsub(/[ :]/, '-')
+    datetime = post.css('.date')[0].inner_text.strip.gsub(/[ :]/, '-')
 
-    file_name = "#{datetime}-#{pinyin.to_permlink(title)}.md"
+    file_name = "../source/writings/#{datetime}-#{pinyin.to_permlink(title)}.md"
 
     tags = []
-    (post/'p.tags a').each { |tag| tags << tag.inner_text }
+    post.css('p.tags a').each { |tag| tags << tag.inner_text }
 
     unless File.exist?(file_name)
       File.open(file_name, "w") do |f|
         f.puts "---\r\ntitle: #{title}\r\ntags: #{tags.join ', '}\r\n---\r\n"  
-        f.puts DownmarkIt.to_markdown((post/'.rich-content').inner_html, 'utf-8').gsub(/\r\n\r\n\r\n|\n\n\n/, '\r\n')
+        f.puts DownmarkIt.to_markdown(post.css('.rich-content')[0].to_xml, 'utf-8').gsub(/\r\n\r\n\r\n|\n\n\n/, '\r\n')
       end
     end
   end
 
-  next_page_link = (doc/'.pageturn a.next_page') 
+  next_page_link = doc.css('.pageturn a.next_page') 
 
   if next_page_link.size > 0
-    next_page_link = next_page_link[0].attr('href')
+    next_page_link = next_page_link[0].attr('href').value
     break if next_page_link == stop_link
-    doc = Nokogiri::HTML(open(next_page_link).read)
+    doc = Oga.parse_html(open(next_page_link).read)
   else
     break
   end
