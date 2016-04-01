dc7d9f6a7e3808ce4e8e4c1be04eddd9c07b70f2
utensil <utensilcandel@gmail.com>
2016-04-01 22:20:29 +0800

    Adjust deplay.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# check out master to "build" and clean up
-
-git clone --depth=50 --branch=master git://github.com/$GH_REPO build
-(
-  cd build
-  # remove all files except README.md, .git and .travis.yml
-  ls -1|grep -v README.md|xargs rm -rf
-)
+mkdir build
+mv .git build/
+(cd build; git checkout -b master origin/master)
 
 # build site to "build"
 
@@ -27,13 +22,12 @@ git config --global push.default simple
 git branch -a
 echo "STATUS"
 git status
-git remote rename origin old
 echo "remotes pre pre-authorized remote url"
 git remote -v
-git remote add origin $REPO_URL
+git remote add site $REPO_URL
 git config remote.origin.url $REPO_URL
 
 git add --all -f .
 git rm $(git ls-files --deleted|grep -v README.md)
 git commit -m "$lastCommit"
-git push origin HEAD:master
+git push site HEAD:master
