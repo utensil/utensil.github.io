89b2d60de7b35670769f7abedbee23917854c9e1
utensil <utensilcandel@gmail.com>
2016-04-02 15:11:20 +0800

    Adjust deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ mkdir build
 mv .git build/
 cd build
 git checkout -b master origin/master
+git branch -a
 cd ../
 
 mv build/.git ./
@@ -19,13 +20,11 @@ export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 git config --global push.default simple
-git branch -a
 echo "STATUS"
 git status
-echo "remotes pre pre-authorized remote url"
 git remote -v
 git config remote.origin.url $REPO_URL
-git pull origin HEAD:master
+git fetch origin HEAD:master
 
 git add --all -f .
 git rm $(git ls-files --deleted|grep -v README.md)
